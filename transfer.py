a7c9eddc88f5aa9f8307cef20933545d35cfb049
TeaganLi <tgli0809@gmail.com>
2018-06-22 15:08:40 +0800

    policy graident with temperature 0.5

--- transfer.py
+++ transfer.py
@@ -51,96 +51,6 @@ class SoftmaxPolicy:
     def sample(self, phi):
         return int(self.rng.choice(self.weights.shape[1], p=self.pmf(phi)))
 
-class SigmoidTermination:
-    def __init__(self, rng, nfeatures):
-        self.rng = rng
-        self.weights = np.zeros((nfeatures,))
-
-    def pmf(self, phi):
-        return expit(np.sum(self.weights[phi]))
-
-    def sample(self, phi):
-        return int(self.rng.uniform() < self.pmf(phi))
-
-    def grad(self, phi):
-        terminate = self.pmf(phi)
-        return terminate*(1. - terminate), phi
-
-class IntraOptionQLearning:
-    def __init__(self, discount, lr, terminations, weights):
-        self.lr = lr
-        self.discount = discount
-        self.terminations = terminations
-        self.weights = weights
-
-    def start(self, phi, option):
-        self.last_phi = phi
-        self.last_option = option
-        self.last_value = self.value(phi, option)
-
-    def value(self, phi, option=None):
-        if option is None:
-            return np.sum(self.weights[phi, :], axis=0)
-        return np.sum(self.weights[phi, option], axis=0)
-
-    def advantage(self, phi, option=None):
-        values = self.value(phi)
-        advantages = values - np.max(values)
-        if option is None:
-            return advantages
-        return advantages[option]
-
-    def update(self, phi, option, reward, done):
-        # One-step update target
-        update_target = reward
-        if not done:
-            current_values = self.value(phi)
-            termination = self.terminations[self.last_option].pmf(phi)
-            update_target += self.discount*((1. - termination)*current_values[self.last_option] + termination*np.max(current_values))
-
-        # Dense gradient update step
-        tderror = update_target - self.last_value
-        self.weights[self.last_phi, self.last_option] += self.lr*tderror
-
-        if not done:
-            self.last_value = current_values[option]
-        self.last_option = option
-        self.last_phi = phi
-
-        return update_target
-
-class IntraOptionActionQLearning:
-    def __init__(self, discount, lr, terminations, weights, qbigomega):
-        self.lr = lr
-        self.discount = discount
-        self.terminations = terminations
-        self.weights = weights
-        self.qbigomega = qbigomega
-
-    def value(self, phi, option, action):
-        return np.sum(self.weights[phi, option, action], axis=0)
-
-    def start(self, phi, option, action):
-        self.last_phi = phi
-        self.last_option = option
-        self.last_action = action
-
-    def update(self, phi, option, action, reward, done):
-        # One-step update target
-        update_target = reward
-        if not done:
-            current_values = self.qbigomega.value(phi)
-            termination = self.terminations[self.last_option].pmf(phi)
-            update_target += self.discount*((1. - termination)*current_values[self.last_option] + termination*np.max(current_values))
-
-        # Update values upon arrival if desired
-        tderror = update_target - self.value(self.last_phi, self.last_option, self.last_action)
-        self.weights[self.last_phi, self.last_option, self.last_action] += self.lr*tderror
-
-        self.last_phi = phi
-        self.last_option = option
-        self.last_action = action
-
 class ValueFunctionLearning:
     def __init__(self, discount, lr, nfeatures):
         self.discount = discount
@@ -167,27 +77,6 @@ class ValueFunctionLearning:
         self.last_phi = phi
         return tderror
 
-class TerminationGradient:
-    def __init__(self, terminations, critic, lr):
-        self.terminations = terminations
-        self.critic = critic
-        self.lr = lr
-
-    def update(self, phi, option):
-        magnitude, direction = self.terminations[option].grad(phi)
-        self.terminations[option].weights[direction] -= \
-                self.lr*magnitude*(self.critic.advantage(phi, option))
-
-class IntraOptionGradient:
-    def __init__(self, option_policies, lr):
-        self.lr = lr
-        self.option_policies = option_policies
-
-    def update(self, phi, option, action, critic):
-        actions_pmf = self.option_policies[option].pmf(phi)
-        self.option_policies[option].weights[phi, :] -= self.lr*critic*actions_pmf
-        self.option_policies[option].weights[phi, action] += self.lr*critic
-
 class PolicyGradient:
     def __init__(self, policy, lr):
         self.lr = lr
@@ -198,23 +87,6 @@ class PolicyGradient:
         self.policy.weights[phi, :] -= self.lr*critic*actions_pmf
         self.policy.weights[phi, action] += self.lr*critic
 
-class OneStepTermination:
-    def sample(self, phi):
-        return 1
-
-    def pmf(self, phi):
-        return 1.
-
-class FixedActionPolicies:
-    def __init__(self, action, nactions):
-        self.action = action
-        self.probs = np.eye(nactions)[action]
-
-    def sample(self, phi):
-        return self.action
-
-    def pmf(self, phi):
-        return self.probs
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
