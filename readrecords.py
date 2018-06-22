16cf8246c04f7767a6c3c1828d9a65bd06bd01fb
TeaganLi <tgli0809@gmail.com>
2018-06-22 09:48:00 +0800

    Original version

---
+++ readrecords.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.load('optioncritic-fourrooms-baseline_True-discount_0.99-epsilon_0.01-lr_critic_0.5-lr_intra_0.25-lr_term_0.25-nepisodes_250-noptions_4-nruns_100-nsteps_1000-primitive_False-temperature_0.01.npy')
+data = np.mean(data, axis=0)
+step = data[:,0]
+avgduration = data[:,1]
+
+plt.subplot(2,1,1)
+plt.plot(step)
+plt.title('The average step length.')
+
+plt.subplot(2,1,2)
+plt.plot(avgduration)
+plt.title('The average duration of each option.')
+plt.show()
